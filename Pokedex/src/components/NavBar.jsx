80ac63bd903acb09b5512b38a330a29844a25a72
Laure Graves <graveslaure@gmail.com>
2023-04-03 16:31:16 +0200

    bouton par pokemon

--- Pokedex/src/components/NavBar.jsx
+++ Pokedex/src/components/NavBar.jsx
@@ -1,24 +1,29 @@
 function NavBar({pokemonIndex, setPokemonIndex, pokemonList}){
+  const handleClickSet=(pokemonIndex)=>{
+    setPokemonIndex(pokemonIndex);
+  };
  
-  const handlePreviewClick =() => {
-    if (pokemonIndex>0){
-    setPokemonIndex (pokemonIndex -1);
-  }
-};
+//   const handlePreviewClick =() => {
+//     if (pokemonIndex>0){
+//     setPokemonIndex (pokemonIndex -1);
+//   }
+// };
   
-  const handleNextClick =() => {
-    if (pokemonIndex < pokemonList.length - 1) {
-    setPokemonIndex (pokemonIndex + 1)
-  }
-}
- 
+//   const handleNextClick =() => {
+//     if (pokemonIndex < pokemonList.length - 1) {
+//     setPokemonIndex (pokemonIndex + 1)
+//   }
+// }
 
   return(
     <div>
-      <button onClick={handlePreviewClick}>précédent</button>
-      <button onClick={handleNextClick}>suivant</button>
+    {pokemonList.map((pokemon,pokemonIndex)=>(
+      <button key={pokemon.name} onClick={()=> handleClickSet(pokemonIndex)}>
+        {pokemon.name}</button>
+    ))
+    }
     </div>
-  )
-}
-
+    );
+    }
+  
 export default NavBar;
